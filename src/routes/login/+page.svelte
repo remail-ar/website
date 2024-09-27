1b9c6eacf444ff02477e191872766c2cda1d1c64
Gonzalo Villanueva <gonzalomvillanueva@gmail.com>
2024-09-27 14:49:45 -0300

    feat: - register page added - cusomtValidators.js created to make register form work - POST method added into register form

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,12 +2,15 @@
 	import { useForm, validators, HintGroup, Hint, email, required } from "svelte-use-form";
 
 	const form = useForm();
+
+
+	let data = undefined
 </script>
 
 <main>
 
 
-	<form use:form method="POST" action="?/login">
+	<form use:form method="POST" action="/login">
 		<h1>Login</h1>
 
 		<input type="email" name="email" use:validators={[required, email]} />
@@ -19,11 +22,9 @@
 		<input type="password" name="password" use:validators={[required]} />
 		<Hint for="password" on="required">This is a mandatory field</Hint>
 
-		<button disabled={!$form.valid}>Login</button>
+		<button disabled={!$form.valid} formaction="/login">Login</button>
 		<button formaction="?/register">Register</button>
 	</form>
-
-	<form method="POST" action="?/login"></form>
 
 	<a href="/register">You don't have an account? Sign up</a>
 
